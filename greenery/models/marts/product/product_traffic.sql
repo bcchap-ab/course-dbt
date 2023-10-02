635b752f3acb585b5e0268df98cff9081bf17175
Bottreypich [Cassey] Chap <cchap@actblue.com>
2023-10-02 01:31:03 +0000

    Add product_id for product_traffic.sql

--- greenery/models/marts/product/product_traffic.sql
+++ greenery/models/marts/product/product_traffic.sql
@@ -18,11 +18,12 @@ events as (
 )
 select 
     products.name
+    , products.product_id
     , sum(case when events.event_type = 'page_view' then 1 else 0 end) as product_page_views
     , sum(case when events.event_type = 'add_to_cart' then 1 else 0 end) as product_add_to_cart
     , count(distinct order_items.order_id) as product_checkout
 from products
 left join events on events.product_id = products.product_id
 left join order_items on order_items.product_id = products.product_id
-group by 1
+group by 1, 2
 order by 1 asc
